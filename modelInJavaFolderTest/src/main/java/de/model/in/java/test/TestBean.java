b096f0c13076a421c380bde93b7887bfdd799d37
Sascha Petersen <Sascha.Petersen@dataport.de>
2017-06-27 09:19:57 +0200

    Initial commit

---
+++ modelInJavaFolderTest/src/main/java/de/model/in/java/test/TestBean.java
@@ -0,0 +1,19 @@
+package de.model.in.java.test;
+
+public class TestBean {
+	private String name;
+	private String firstname;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+}
